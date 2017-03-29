05d60b1d2fd5ea2daffe5989665ce3d524bfc59a
chaiweiwei <1119191759@qq.com>
2017-03-29 16:39:44 +0800

    Initial Commit

---
+++ SepecTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SepecTest
+//
+//  Created by chaiweiwei on 2017/3/29.
+//  Copyright © 2017年 chaiweiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
